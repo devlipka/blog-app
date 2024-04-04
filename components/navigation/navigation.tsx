787df07892bef73e49fd6e4e975d39734a71b7fb
devlipka <alanneck13@gmail.com>
2024-04-04 16:06:29 +0300

    Innitial structure

---
+++ components/navigation/navigation.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+const Navigation = () => {
+  return (
+    <div>
+      <Link href="">Explorer</Link>
+      {/*Category Links*/}
+      <nav>
+        <ul>
+          <li>
+            <Link href="/cities">Cities</Link>
+          </li>
+          <li>
+            <Link href="/experiences">Experiences</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+export default Navigation;
