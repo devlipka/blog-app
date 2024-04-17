da7782681a61f03ba48d884ad6924dd0748925b9
devlipka <alanneck13@gmail.com>
2024-04-17 16:11:25 +0300

    Debugg

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: NextRequest) {
   const token = request.nextUrl.searchParams.get("token");
   // If there is no token, return a 401
   if (!token || token !== process.env.ADMIN_TOKEN)
-    return NextResponse.json({ error: "Not authorised" }, { status: 401 });
+    return NextResponse.json({ error: "Not authorised" });
   // Revalidate All Posts
   revalidatePath(`/[lang]/post/[slug]`);
   // Revalidate All Categories
