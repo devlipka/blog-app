05d5d1eccf65b279e0985a4a563bf590836fce77
devlipka <alanneck13@gmail.com>
2024-04-17 15:53:53 +0300

    Update revalidate func

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,12 +1,11 @@
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
-import { NextApiResponse } from "next";
 
-export async function GET(request: NextRequest, res: NextApiResponse) {
+export async function GET(request: NextRequest) {
   const token = request.nextUrl.searchParams.get("token");
   // If there is no token, return a 401
   if (!token || token !== process.env.ADMIN_TOKEN)
-    return res.status(200).json({ error: "Not authorised" });
+    return NextResponse.json({ error: "Not authorised" }, { status: 401 });
   // Revalidate All Posts
   revalidatePath(`/[lang]/post/[slug]`);
   // Revalidate All Categories
@@ -14,5 +13,5 @@ export async function GET(request: NextRequest, res: NextApiResponse) {
   // Revalidate All Languages
   revalidatePath(`/[lang]`);
 
-  return res.json({ revalidated: true, now: Date.now() });
+  return NextResponse.json({ revalidated: true, now: Date.now() });
 }
